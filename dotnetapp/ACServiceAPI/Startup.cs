004a9b5070eef89c5a4e35c0ceee191eb7d119b7
venkatesh42 <72272010+venkatesh42@users.noreply.github.com>
2022-03-31 14:18:01 +0530

    Update Startup.cs
    Startup file added

--- dotnetapp/ACServiceAPI/Startup.cs
+++ dotnetapp/ACServiceAPI/Startup.cs
@@ -1,16 +1,20 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ACServiceAPI.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ACServiceAPI
 {
@@ -32,19 +36,44 @@ namespace ACServiceAPI
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ACServiceAPI", Version = "v1" });
             });
+
+            services.AddDbContext<ACServiceDbContext>(options =>
+           options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer
+                (opt =>
+                {
+                    opt.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidIssuer = Configuration["Jwt:Isuuer"],
+                        ValidAudience = Configuration["Jwt:Audience"],
+                        ValidateIssuerSigningKey=true,
+                        IssuerSigningKey =new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:key"]))
+                        
+                    };
+            });
+
+                
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(options =>
+            options.WithOrigins("http://localhost:4200")
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ACServiceAPI v1"));
             }
-
-            app.UseHttpsRedirection();
 
             app.UseRouting();
 
