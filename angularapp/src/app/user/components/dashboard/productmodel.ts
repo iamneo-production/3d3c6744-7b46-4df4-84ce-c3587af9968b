0870e333fe6918dd95e5996b7d4b7c927fc480a6
VIJAY <61859349+vijay-itachi@users.noreply.github.com>
2022-03-31 12:38:50 +0530

    Update productmodel.ts
    dashboard service added

---
+++ angularapp/src/app/user/components/dashboard/productmodel.ts
@@ -0,0 +1,12 @@
+export class ProductModel{
+    productID: number=0;
+    productName: string='';
+    productModelNo: string='';
+    dateOfPurchase: Date;
+    contactNumber: string='';
+    problemDescription: string='';
+    availableSlots: string='';
+    bookingDate:Date;
+    servicecentername:string='';
+    userId:number;
+}
