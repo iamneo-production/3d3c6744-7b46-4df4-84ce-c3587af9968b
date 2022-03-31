cc0386ec460fadface555a82c8efd442bddebcda
HarshithaDogiparthi <97834262+HarshithaDogiparthi@users.noreply.github.com>
2022-03-31 12:42:15 +0530

    Update login.component.ts
    user login added

--- angularapp/src/app/user/components/login/login.component.ts
+++ angularapp/src/app/user/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup,FormControlName, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginService } from './login.service';
+import { LoginModel } from './loginmodel';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  userId:any;
+  public Obj = new LoginModel();
+ 
+  constructor(private router : Router,public service:LoginService)
+   {    }
 
   ngOnInit(): void {
+  }
+  LoginForm =new FormGroup({
+    'email' : new FormControl('',[Validators.required,Validators.pattern('^[a-zA-Z0-9]+[.+-_]{0,1}[a-zA-Z0-9]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3}){0,1}')]),
+    'password' : new FormControl('',[Validators.required,Validators.pattern('^[a-zA-Z0-9]{8,}')])
+  })
+
+  responsedata:any;
+  Login(){ 
+    this.Obj.email = this.LoginForm.value.email;
+    this.Obj.password = this.LoginForm.value.password;
+
+    this.service.login(this.Obj).subscribe(
+      res=>{
+        this.responsedata=res;
+      this.userId=this.responsedata.userId;
+     this.service.set( this.userId);
+        alert("Login Succesfull");
+        //console.log(res);
+        this.service.isLoggedInToken= this.responsedata.jwtToken;
+       //console.log(this.responsedata.jwtToken);
+
+      localStorage.setItem('token',this.responsedata.jwtToken);
+     localStorage.setItem('userRole',this.responsedata.userType);
+     // console.log( this.service.isLoggedInToken);
+        this.LoginForm.reset();
+        this.router.navigate(['user/homepage']);
+      },
+      err =>{ 
+        alert("Login failed");
+        this.LoginForm.reset();
+        console.log(err);
+      }
+    );
+  }
+
+  get email(){
+    return this.LoginForm.get('email');
+  }
+
+  get password(){
+    return this.LoginForm.get('password');
   }
 
 }
