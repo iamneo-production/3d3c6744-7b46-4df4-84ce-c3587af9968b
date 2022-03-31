ab1d0470e4866cb53db9095148e04688d930760d
kopparapumanikanta <70697230+kopparapumanikanta@users.noreply.github.com>
2022-03-31 12:46:49 +0530

    Update servicecentermodel.ts

---
+++ angularapp/src/app/user/components/homepage/servicecentermodel.ts
@@ -0,0 +1,9 @@
+export class ServiceCenterModel{
+    serviceCenterID : number=0;
+    serviceCenterName: string='';
+    serviceCenterPhone: string='';
+    serviceCenterAddress: string='';
+    serviceCenterImageUrl: string='';
+    serviceCentermailId: string='';
+    serviceCenterDescription: string='';
+}
